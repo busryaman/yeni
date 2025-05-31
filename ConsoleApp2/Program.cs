a25a0702d94b2507308d4978dbca1080f7f41a1c
Büşra <busraymn3734@gmail.com>
2025-05-31 20:09:33 +0300

    password kısmı yapıldı

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("hi");
+        Console.WriteLine("choose what do you want (as number)");
+        Console.WriteLine("1- sign in");
+        Console.WriteLine("2- sign up");
+        int preference1=int.Parse(Console.ReadLine());
+        switch (preference1)
+        { case 1:
+            Console.WriteLine("enter your username ");
+            string username= Console.ReadLine();
+            Console.WriteLine("enter your password");
+            string password = Console.ReadLine();
+            break;
+        }
     }
 }
